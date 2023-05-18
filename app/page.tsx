fd2e9b478b0cca56dc201e572d5790119c5ca06a
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-05-18 09:14:30 -0400

    cannot find SupabseListener

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import { supabase } from '@/lib/supabaseClient';
+import AuthComponent from '@/components/AuthComponent';
 
 export default async function Home() {
-  let { data } = await supabase.from('countries').select()
+  let { users }: any = await supabase.from('users').select()
 
   return (
     <main>
       <h1 className='text-3xl font-bold'>Hello World</h1>
       <ul className='list-disc list-inside'>
-        {data?.map((item: any, index: any) => (
+        {users?.map((item: any, index: any) => (
           <li key={index}>{item.name}</li>
         ))}
       </ul>
+      <AuthComponent />
     </main>
   )
 }
